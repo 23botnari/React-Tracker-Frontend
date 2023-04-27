8d1e542ce5ce6b5af7ff64b556c0d263cbe153cf
Botnari Ion <ibotnari2414@gmail.com>
2023-04-28 00:13:27 +0300

    Login Design Updated

--- src/components/containers/AppRoutes/AppRoutes.js
+++ src/components/containers/AppRoutes/AppRoutes.js
@@ -1,14 +1,15 @@
+import React from "react";
+import {  Route, Routes } from "react-router-dom";
 import Login from "../../containers/Login/Login";
 import Dashboard from "../../../pages/Dashboard/Dashboard";
 import Companies from "../../../pages/Companies/Companies";
 import Phones from "../../../pages/Phones/Phones";
-import {  Route, Routes } from "react-router-dom";
 
 const AppRoutes = () => {
   return (
       <Routes>
         <Route index path="/dashboard" element={<Dashboard />} />
-        <Route path="/auth/login" element={<Login />} />
+        {/* <Route path="/auth/login" element={<Login />} /> */}
         <Route path="/companies" element={<Companies />} />
         <Route path="/phones" element={<Phones />} />
       </Routes>
--- src/components/containers/Login/Login.js
+++ src/components/containers/Login/Login.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import "./Login.css";
+import Logo from "./../../../assets/logo192.png"
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
-import { useAppContext } from "../../../lib/contextLib";
 import PropTypes from 'prop-types';
+import { useNavigate } from "react-router-dom";
+
 async function loginUser(credentials) {
   return fetch('http://localhost:4000/auth/login', {
     method: 'POST',
@@ -14,34 +16,67 @@ async function loginUser(credentials) {
   })
     .then(data => data.json())
  }
+ 
 export default function Login({setToken}) {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+  const navigate= useNavigate()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const handleSubmit = async e => {
+    // window.location.href('http://localhost:3000/')
     e.preventDefault();
     const token = await loginUser({
       email,
       password
     });
     setToken(token);
+     navigate("/dashboard")
   }
   return (
-    <div className="login-wrapper">
-    <h1>Please Log In</h1>
-    <form onSubmit={handleSubmit}>
-      <label>
-        <p>Username</p>
-        <input type="text" onChange={e => setEmail(e.target.value)}/>
-      </label>
-      <label>
-        <p>Password</p>
-        <input type="password" onChange={e => setPassword(e.target.value)}/>
-      </label>
-      <div>
-        <button type="submit">Submit</button>
+    <div className="flex align-items-center justify-content-center" >
+      <div
+        className="surface-card p-4 shadow-2 border-round w-full lg:w-6"
+        style={{ margin: "100px" }}
+      >
+        <form onSubmit={handleSubmit}>
+          <div className="text-center mb-5">
+            <img src={Logo} alt="hyper" height={50} className="mb-3" />
+          </div>
+
+          <div>
+            <label htmlFor="email" className="block text-900 font-medium mb-2">
+              Email
+            </label>
+            <InputText
+              id="email"
+              type="text"
+              placeholder="Email address"
+              className="w-full mb-3"
+              onChange={e => setEmail(e.target.value)}
+            />
+
+            <label
+              htmlFor="password"
+              className="block text-900 font-medium mb-2"
+              >
+              Password
+            </label>
+            <InputText
+              id="password"
+              type="password"
+              placeholder="Password"
+              className="w-full mb-3"
+              onChange={e => setPassword(e.target.value)}
+            />
+
+            <div className="flex align-items-center justify-content-between mb-6">
+              <a className="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer"></a>
+            </div>
+
+            <Button type="submit" label="Sign In" icon="pi pi-user" className="w-full" />
+          </div>
+        </form>
       </div>
-    </form>
-  </div>
+    </div>
   );
 };
 Login.propTypes = {
