8bf7ad95461917da00a49f25d78a8b99a8cad1de
Botnari Ion <ibotnari2414@gmail.com>
2023-05-22 16:29:49 +0300

    Roles

--- src/components/containers/SideMenu/SideMenu.js
+++ src/components/containers/SideMenu/SideMenu.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import "./SideMenu.scss";
 import Logo from "../../../assets/logo192.png";
 import { SideMenuData } from "./SideMenuData";
@@ -9,7 +8,6 @@ import {
   setPanelTitle,
   setPanelType,
 } from "../../../redux/actions/sidePanelActions";
-
 import { setIsExpanded } from "../../../redux/actions/sideMenuActions";
 import useToken from "../Login/useToken";
 
@@ -17,9 +15,60 @@ const SideMenu = () => {
   const { isExpanded } = useSelector((state) => state.SideMenuReducer);
   const dispatch = useDispatch();
   const { setToken } = useToken();
+
+  const userRole = "driver"; 
+
   const refreshPage = () => {
     document.location.reload(false);
   };
+
+  const renderMenuItems = () => {
+    return SideMenuData.map((val, key) => {
+      if (userRole === "operator" || (userRole === "driver" && val.label !== "Companies" && val.label !== "Add Trip"&& val.label !== "Phones")) {
+        return (
+          <li
+            key={key}
+            className="row"
+            id={window.location.pathname === val.link ? "active" : ""}
+            onClick={() => {
+              if (val.link) {
+                window.location.pathname = val.link;
+              } else {
+                switch (val.label) {
+                  case "Trips":
+                    dispatch(setIsOpen(true));
+                    dispatch(setPanelType("Trips"));
+                    dispatch(setPanelTitle("Trips"));
+                    if (window.location.pathname !== "/dashboard")
+                      window.location.pathname = "/dashboard";
+                    break;
+                  case "Add Trip":
+                    dispatch(setIsOpen(true));
+                    dispatch(setPanelType("AddTrip"));
+                    dispatch(setPanelTitle("Add Trip"));
+                    if (window.location.pathname !== "/dashboard")
+                      window.location.pathname = "/dashboard";
+                    break;
+                  default:
+                    break;
+                }
+              }
+              if (val.label === "Log Out") {
+                window.location.pathname = "/auth/login";
+                setToken(null);
+              }
+            }}
+          >
+            <i id="icon" className={val.icon}></i>
+            {!!isExpanded ? <span id="label">{val.label}</span> : ""}
+          </li>
+        );
+      } else {
+        return null;
+      }
+    });
+  };
+
   return (
     <>
       <div className={`SideMenu ${!!isExpanded ? "expanded" : ""}`}>
@@ -33,49 +82,8 @@ const SideMenu = () => {
           <div className="SideMenu__Title">React Tracker</div>
         </div>
 
-        <ul className="SideMenu__List">
-          {SideMenuData.map((val, key) => {
-            return (
-              <li
-                key={key}
-                className="row"
-                id={window.location.pathname === val.link ? "active" : ""}
-                onClick={() => {
-                  if (val.link) {
-                    window.location.pathname = val.link;
-                  } else
-                    switch (val.label) {
-                      case "Trips":
-                        dispatch(setIsOpen(true));
-                        dispatch(setPanelType("Trips"));
-                        dispatch(setPanelTitle("Trips"));
-                        if (window.location.pathname !== "/dashboard")
-                          window.location.pathname = "/dashboard";
-                        break;
-                      case "Add Trip":
-                        dispatch(setIsOpen(true));
-                        dispatch(setPanelType("AddTrip"));
-                        dispatch(setPanelTitle("Add Trip"));
-                        if (window.location.pathname !== "/dashboard")
-                          window.location.pathname = "/dashboard";
-                        break;
-                   
-                      default:
-                        break;
-                    }
-                    if (val.label ==="Log Out"){
-                      window.location.pathname = "/auth/login";
-                      setToken(null);
-                  
-                    }
-                }}
-              >
-                <i id="icon" className={val.icon}></i>
-                {!!isExpanded ? <span id="label">{val.label}</span> : ""}
-              </li>
-            );
-          })}
-        </ul>
+        <ul className="SideMenu__List">{renderMenuItems()}</ul>
+
         <div
           className="SideMenu__button"
           onClick={() => {
@@ -90,4 +98,5 @@ const SideMenu = () => {
     </>
   );
 };
+
 export default SideMenu;
